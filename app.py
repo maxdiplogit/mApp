bfa36dc466874f26ab2b54c67641373073c33fac
maxdiplogit <maxdiplogit@gmail.com>
2024-12-28 22:11:43 +0530

    implement basic desktop app in python using PyQt

--- app.py
+++ app.py
@@ -11,6 +11,6 @@ app = QApplication(sys.argv)
 # Create a Qt widget, which will be our window.
 window = QWidget()
 window.show()  # IMPORTANT!!!!! Windows are hidden by default.
-
+    
 # Start the event loop.
 app.exec()
